2731a3dd1bc0b9357ef8925e818b69c4ac305a8c
Maanucoder <86370615+Maanucoder@users.noreply.github.com>
2021-09-03 12:47:10 +0530

    Add files via upload

---
+++ pelindrom.py
@@ -0,0 +1,27 @@
+n=input()
+while True:
+		l=[]
+		n=int(n)
+		n=n+1
+		for item in str(n):
+			l.append(item)
+		z=l.copy()
+		r=l[::-1]
+		j1="".join(l)
+		j2="".join(r)
+	
+		if j1==j2:
+				print(j2)
+				break
+
+
+				
+			
+		
+		
+			
+	
+	
+		
+		
+	
